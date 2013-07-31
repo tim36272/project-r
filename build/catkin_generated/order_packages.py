68121470c42bf10b7d2bc52811015396ad94d69d
Tim Sweet <timothy.l.sweet@gmail.com>
2013-07-31 11:08:45 -0700

    Removed extra folders not related to source

--- build/catkin_generated/order_packages.py
+++
@@ -1,4 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/tim-dev/Dropbox/Workspaces/research/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
